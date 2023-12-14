36c878befb0d45cca7c74ebd1d5c9720c4de8cce
pompidou41 <musaev5115@gmail.com>
2023-12-14 11:38:12 +0300

    basic markup

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,28 +1,29 @@
 const React = require('react');
-const Navbar = require('./Navbar');
+const Header = require('./Header');
 const Footer = require('./Footer');
 
 function Layout({ title, children, user }) {
   return (
-    <html lang='ru'>
+    <html lang="ru">
       <head>
         <title>{title}</title>
-        <link rel='stylesheet' href='/styles/style.css' />
+        <link rel="stylesheet" href="/styles/style.css" />
         <link
-          href='https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css'
-          rel='stylesheet'
-          integrity='sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC'
-          crossOrigin='anonymous'
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
+          rel="stylesheet"
+          integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
+          crossOrigin="anonymous"
         />
         <script
-          src='https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js'
-          integrity='sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM'
-          crossOrigin='anonymous'
+          src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+          integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+          crossOrigin="anonymous"
         />
       </head>
       <body>
-        <Navbar />
+        <Header />
         {children}
+        <Footer />
       </body>
     </html>
   );
--- components/MainPage.jsx
+++ components/MainPage.jsx
@@ -2,10 +2,13 @@ const React = require('react');
 const Layout = require('./Layout');
 
 function MainPage() {
-    return(
-        <Layout>
-            
-        </Layout>
-    )
+  return (
+    <Layout>
+      <div className="container greeting"></div>
+      <div className="container watches"></div>
+      <div className="container clients"></div>
+      <div className="container contacts"></div>
+    </Layout>
+  );
 }
-module.exports = MainPage
+module.exports = MainPage;
